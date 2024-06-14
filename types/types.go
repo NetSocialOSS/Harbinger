5b8ad507532b9fb9081aa4a162ed902752cf1d5e
Maya <foenx0315@gmail.com>
2024-06-14 07:27:07 +0530

    Update types.go

--- types/types.go
+++ types/types.go
@@ -108,36 +108,6 @@ type Partner struct {
 	Link   string `json:"link,omitempty" bson:"link,omitempty"`
 }
 
-type ImgbbImage struct {
-	Filename  string `json:"filename"`
-	Name      string `json:"name"`
-	Mime      string `json:"mime"`
-	Extension string `json:"extension"`
-	Url       string `json:"url"`
-}
-
-type ImgbbResponse struct {
-	Data    ImgbbResponseData `json:"data"`
-	Success bool              `json:"success"`
-	Status  int               `json:"status"`
-}
-
-type ImgbbResponseData struct {
-	ID string `json:"id"`
-
-	DisplayURL string `json:"display_url"`
-	DeleteURL  string `json:"delete_url"`
-
-	Expiration string `json:"expiration"`
-
-	Height string `json:"height"`
-	Width  string `json:"width"`
-
-	Image  ImgbbImage `json:"image"`
-	Thumb  ImgbbImage `json:"thumb"`
-	Medium ImgbbImage `json:"medium"`
-}
-
 /*
  * ==========================
  * Configuration Types: not suggested to mess with!!
