abac17cbff81f3504afe6eb44840307f1af67073
Maya <foenx0315@gmail.com>
2024-06-20 08:10:43 +0530

    Update Follow.go
    added to be followed stuff its like when you follow a user it also adds i to your account that you've followed them earlier it didnt did that

--- routes/Follow.go
+++ routes/Follow.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -22,8 +23,8 @@ func FollowUser(c *fiber.Ctx) error {
 	userCollection := db.Database("SocialFlux").Collection("users")
 
 	// Find the user to be followed
-	var user bson.M
-	err := userCollection.FindOne(context.TODO(), bson.M{"username": username}).Decode(&user)
+	var userToBeFollowed bson.M
+	err := userCollection.FindOne(context.TODO(), bson.M{"username": username}).Decode(&userToBeFollowed)
 	if err != nil {
 		if err == mongo.ErrNoDocuments {
 			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "User not found"})
@@ -31,12 +32,35 @@ func FollowUser(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Error finding user"})
 	}
 
+	// Find the follower user
+	var followerUser bson.M
+	followerObjectID, err := primitive.ObjectIDFromHex(followerID)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid follower ID"})
+	}
+
+	err = userCollection.FindOne(context.TODO(), bson.M{"_id": followerObjectID}).Decode(&followerUser)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "Follower not found"})
+		}
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Error finding follower"})
+	}
+
 	// Add follower's user ID to the user's followers list
-	filter := bson.M{"username": username}
-	update := bson.M{"$addToSet": bson.M{"followers": followerID}}
-	_, err = userCollection.UpdateOne(context.TODO(), filter, update, options.Update().SetUpsert(false))
+	followedUserFilter := bson.M{"username": username}
+	followedUserUpdate := bson.M{"$addToSet": bson.M{"followers": followerID}}
+	_, err = userCollection.UpdateOne(context.TODO(), followedUserFilter, followedUserUpdate, options.Update().SetUpsert(false))
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Error updating followers list"})
+	}
+
+	// Add followed user's ID to the follower's following list
+	followerUserFilter := bson.M{"_id": followerObjectID}
+	followerUserUpdate := bson.M{"$addToSet": bson.M{"following": userToBeFollowed["_id"].(primitive.ObjectID).Hex()}}
+	_, err = userCollection.UpdateOne(context.TODO(), followerUserFilter, followerUserUpdate, options.Update().SetUpsert(false))
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Error updating following list"})
 	}
 
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{"message": "Successfully followed user"})
