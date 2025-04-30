19002359c6b400596fc59d8a3220b9f44709013f
Miya25 <foenx0315@gmail.com>
2025-04-30 18:08:11 +0530

    fixes

--- seed/partner.sql
+++ seed/partner.sql
@@ -2,7 +2,6 @@ CREATE TABLE partner (
     id uuid PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
     banner text,
     logo text NOT NULL,
-    createdat timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
     title text NOT NULL,
     text text NOT NULL,
     link text NOT NULL
